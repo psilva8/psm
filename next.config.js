31babc7fc7234a63bc004f17adcf2f7b0872df42
Paul Silva <psilva8@gmail.com>
2025-06-14 13:37:00 -0700

    Update Next.js config for static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'out',
   images: {
+    unoptimized: true,
     domains: ['localhost'],
     remotePatterns: [
       {
